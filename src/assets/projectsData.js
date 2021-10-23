7a235dfd4972dc4467f6fa394f03cca4b5a0fe57
Sagnik Mukherjee <51739091+Sagnik220@users.noreply.github.com>
2021-10-23 19:52:50 +0530

    Add files via upload

---
+++ src/assets/projectsData.js
@@ -0,0 +1,30 @@
+export const projectsData = [
+    {
+        projectName: 'AirWays',
+        projectDescription:
+            `A Machine Learning based Web Application for Predicting the Flight prices `,
+        imageUrl: 'https://www.usnews.com/dims4/USNEWS/ed7b154/2147483647/crop/2000x1334%2B0%2B1/resize/970x647/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2Fb7%2Ff4%2F847d373a4218bcbb9383ff501eaa%2F191004-flightapp-stock.jpg',
+        Application:
+            'https://aviation-price-prediction.herokuapp.com/',
+        projectUrl: 'https://github.com/Sagnik220/Aviation-Price-Prediction',
+    },
+    {
+        projectName: 'FireGuard',
+        projectDescription:
+            'A Machine Learning based Web Application for helping people by predicting the chances of a Forest Fire',
+        imageUrl: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F20%2F2021%2F08%2F12%2Fwildfires-1.jpg',
+        Application:
+            'https://forest-valley-fire-prediction.herokuapp.com/',
+        projectUrl: 'https://github.com/Sagnik220/Forest-Fire-Prediction',
+    },
+    {
+        projectName: 'Unipredict',
+        projectDescription:
+            'A Machine Learning based Web Application for prediction the chances of University Admissions based on GRE,TOEFL and other parameters',
+        imageUrl: 'http://www.milwaukeeindependent.com/wp-content/uploads/2019/03/CollegeAdmissionsPhoto_01.jpg',
+        Application:
+            'https://uni-predict.herokuapp.com/',
+        projectUrl: 'https://github.com/Sagnik220/Graduate-Admission-Predictor/tree/master',
+    },
+   
+];
---
+++ src/components/About/About.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import FooterLink from '../FooterLink/FooterLink';
+import Header from '../Header/Header';
+import aboutVector from './../../assets/about_vector.png';
+import './About.css';
+import aboutAnime from './../../assets/about_anime.gif';
+
+const About = () => {
+    return (
+        <div className='section-container'>
+            <Header
+                heading='About Me.'
+                details='Software Developer | Open Source Contributor | ML Enthusiast | Electronics & Instrumentation Engineer '
+            />
+
+            <div className='about-main'>
+                <div className='about-main-left'>
+                    {/* Sub section 1 */}
+                    <h3 className='about-sub-head'>Student</h3>
+                    <p className='about-details'>
+                        Currently I am Student at Heritage Institute of Technology pursuing my Bachelors in Electronics & Instrumentation, interested in 
+                        Software Development,Machine Learning,Deep Learning.{' '}
+                        <p>
+                            <a
+                                className='about-link-element'
+                                href='https://github.com/Sagnik220'
+                            >
+                                Check out of My Github!
+                            </a>
+                        </p>
+                    </p>
+
+                    {/* Sub section 2 */}
+                    <h3 className='about-sub-head'>Technical Writer</h3>
+                    <p className='about-details'>
+                        I've been writing technical articles for over 1 year related to the latest technologies such as Digital 
+                        Signal Processing, Machine Learning, Data Structure, Algorithms.{' '}
+                        <p>
+                            <a
+                                className='about-link-element'
+                                href='https://auth.geeksforgeeks.org/user/sagnikmukherjee2/articles'
+                            >
+                                My GeeksforGeeks artciles!
+                            </a>
+                        </p>
+                    </p>
+
+                    {/* Sub section 3 */}
+                    <h3 className='about-sub-head'>OpenSource Contributor</h3>
+                    <p className='about-details'>
+                        Being a passionate person interested in open source I would love to collaborate on projects
+                        related to Machine Learning,Deep Learning!{' '}
+                        <p>
+                            <a
+                                className='about-link-element'
+                                href='mailto:sagnik2207@gmail.com'
+                            >
+                                Email me for collaboration!
+                            </a>
+                        </p>
+                    </p>
+                </div>
+
+                <div className='about-main-right'>
+                    <img
+                        src={aboutAnime}
+                        alt='animation'
+                        className='about-anime'
+                    />
+                </div>
+            </div>
+
+            <FooterLink
+                phrase='Check out my '
+                link='projects!'
+                toAdress='/projects'
+            />
+            {/* Vector Frame! */}
+            <div className='vector-frame'>
+                <img src={aboutVector} className='about-vector' alt='about' />
+            </div>
+        </div>
+    );
+};
+
+export default About;
