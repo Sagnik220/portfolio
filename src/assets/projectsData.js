90f85fd07d2a634c33ff0a9e40b8a1cce8fc1d37
Sagnik Mukherjee <51739091+Sagnik220@users.noreply.github.com>
2024-08-15 21:49:49 +0530

    Update projectsData.js

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -1,39 +1,10 @@
 export const projectsData = [
     {
-        projectName: 'AirWays',
+        projectName: 'Serverless API Data Ingestion',
         projectDescription:
-            `A Machine Learning based Web Application for Predicting the Flight prices `,
-        imageUrl: 'https://www.usnews.com/dims4/USNEWS/ed7b154/2147483647/crop/2000x1334%2B0%2B1/resize/970x647/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2Fb7%2Ff4%2F847d373a4218bcbb9383ff501eaa%2F191004-flightapp-stock.jpg',
-        Application:
-            'https://aviation-price-prediction.herokuapp.com/',
-        projectUrl: 'https://github.com/Sagnik220/Aviation-Price-Prediction',
-    },
-    {
-        projectName: 'FreshFind',
-        projectDescription:
-            `Job Listing Website for freshers for offcampus opportunities.The idea behind FreshFind is to make freshers aware of various off-campus Job & Internship opportunity for their career start.`,
-        imageUrl: 'https://i0.wp.com/www.jobopening.in/wp-content/uploads/2019/07/interview-1018333_640-1.png?fit=640%2C481&ssl=1',
-        Application:
-            'https://freshfind.netlify.app/',
-        projectUrl: 'https://github.com/Sagnik220/FreshFind',
-    },
-    {
-        projectName: 'Traffic-Light Automation [OpenCV,Mediapipe,Arduino UNO]',
-        projectDescription:
-            `The idea of the project is to automate traffic light system using Mediapipe,OpenCV and Arduino. We have taken the idea of Handtracking using the Mediapipe Library to count the finger's to light up the LED's.`,
-        imageUrl: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/04/09/10/istock-598086962.jpg?width=990&auto=webp&quality=75&crop=982:726,smart',
-        Application:
-            'https://github.com/Sagnik220/Traffic-Light-Automation',
-        projectUrl: 'https://github.com/Sagnik220/Traffic-Light-Automation',
-    },
-    {
-        projectName: 'FireGuard',
-        projectDescription:
-            'A Machine Learning based Web Application for helping people by predicting the chances of a Forest Fire',
-        imageUrl: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F20%2F2021%2F08%2F12%2Fwildfires-1.jpg',
-        Application:
-            'https://forest-valley-fire-prediction.herokuapp.com/',
-        projectUrl: 'https://github.com/Sagnik220/Forest-Fire-Prediction',
+            'This project involves creating a scalable and cost-effective end-to-end data warehousing and reporting solution using Python, OCI Functions, and OCI Data Integration. The solution includes two Python-based microservices: one for extracting semi-structured data from external APIs, converting it to structured CSV, and storing it in Oracle Object Storage; and another for transforming and loading the data into an Autonomous Data Warehouse (ADW). By leveraging OCI Functions for on-demand invocation and OCI Data Integration for pipeline orchestration, the project ensures a highly efficient and near-zero downtime data pipeline.',
+        imageUrl: 'https://steltix-staging.com/wp-content/uploads/2021/10/Oracle-oci-logo.png',
+        projectUrl: 'https://github.com/Sagnik220/Weather-Data-Engineering',
     },
     {
         projectName: 'Unipredict',
