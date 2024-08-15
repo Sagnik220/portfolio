ba5830e736abe21b72e88f8ae98175ba5861abb1
Sagnik Mukherjee <51739091+Sagnik220@users.noreply.github.com>
2024-08-15 21:59:06 +0530

    Update projectsData.js
    New

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -2,10 +2,11 @@ export const projectsData = [
     {
         projectName: 'Serverless API Data Ingestion',
         projectDescription:
-            'This project involves creating a scalable and cost-effective end-to-end data warehousing and reporting solution using Python, OCI Functions, and OCI Data Integration. The solution includes two Python-based microservices: one for extracting semi-structured data from external APIs, converting it to structured CSV, and storing it in Oracle Object Storage; and another for transforming and loading the data into an Autonomous Data Warehouse (ADW). By leveraging OCI Functions for on-demand invocation and OCI Data Integration for pipeline orchestration, the project ensures a highly efficient and near-zero downtime data pipeline.',
+            'This project involves creating a scalable and cost-effective end-to-end data warehousing and reporting solution using Python, OCI Functions, and OCI Data Integration.The solution includes two Python-based microservices: one for extracting semi-structured data from external APIs, converting it to structured CSV, and storing it in Oracle Object Storage; and another for transforming and loading the data into an Autonomous Data Warehouse (ADW). By leveraging OCI Functions for on-demand invocation and OCI Data Integration for pipeline orchestration, the project ensures a highly efficient and near-zero downtime data pipeline.',
         imageUrl: 'https://steltix-staging.com/wp-content/uploads/2021/10/Oracle-oci-logo.png',
         projectUrl: 'https://github.com/Sagnik220/Weather-Data-Engineering',
     },
+    
     {
         projectName: 'Unipredict',
         projectDescription:
